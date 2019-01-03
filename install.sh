9e1f8330ee1cab1e63088076e916fb0e0a865c04
Daniel Harvey <danieljamesharvey@gmail.com>
2019-01-03 15:18:47 +0000

    Update .vimrc, add .spacemacs and update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 echo -e "Setting up Vim"
 
+# remove old crap
+rm -R ~/dotfiles/.vim/bundle
+cp ~/.vimrc ~/.vimrc.old
+rm ~/.vimrc
+rm -R ~/.vim
+
+# symlink new vim files
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
+
+# install Vundle and Vim plugins
 cd ~/dotfiles/.vim
 mkdir bundle
 cd bundle
@@ -11,5 +20,18 @@ vim -c 'PluginInstall' -c 'qa!'
 
 echo -e "Copying tmux settings"
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+echo -e "Setting up Spacemacs"
+
+# tidy up
+rm -R ~/.emacs.d
+cp ~/.spacemacs ~/.spacemacs.old
+rm ~/.spacemacs
+
+# install spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# copy over new config
+ln -s ~/dotfiles/.spacemacs ~/.spacemacs
 
 echo -e "Probably fine isn't it"
