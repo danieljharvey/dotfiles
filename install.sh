a8ff9f5d8227b8f23058095c98ee37b51e0af53e
Daniel Harvey <danieljamesharvey@gmail.com>
2025-02-21 21:46:04 +0000

    Lazy nvim

--- install.sh
+++ install.sh
@@ -2,31 +2,7 @@
 echo -e "Setting up Vim"
 
 # remove old crap
-rm -R ~/dotfiles/.vim/bundle
-cp ~/.vimrc ~/.vimrc.old
-rm ~/.vimrc
-rm -R ~/.vim
 rm ~/.tmux.conf
-
-# install neovim package manager (why is this so manual?)
-rm -rf ~/.local/share/nvim/site/pack/packer/start
-
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# remove local to break cache
-# rm -rf .vim
-# mkdir .vim
-
-# symlink new vim files
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
-
-# install Vundle and Vim plugins
-cd ~/dotfiles/.vim
-mkdir bundle
-cd bundle
-nvim -c 'PlugInstall' -c 'qa!'
 
 echo -e "Setting up tmux"
 
