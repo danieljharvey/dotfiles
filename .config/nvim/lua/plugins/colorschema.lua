bbfaf7992520158d15920df567d304650790cf28
Daniel Harvey <danieljamesharvey@gmail.com>
2025-02-24 10:51:50 +0000

    Colorscheme

---
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "neanias/everforest-nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
